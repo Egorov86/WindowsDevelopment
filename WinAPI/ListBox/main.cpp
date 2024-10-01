6f7dda62c4de995e50265a95d2154e11b1406ff4
Egoro_st25@top-academy.ru <Egoro_st25@top-academy.ru>
2024-10-01 20:25:51 +0300

    WinApi encoding

---
+++ WinAPI/ListBox/main.cpp
@@ -0,0 +1,7 @@
+//ListBox
+#include<Windows.h>
+
+INT WINAPI WinMain(HINSTANCE histance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
+{
+	return 0;
+}
--- WinAPI/WinAPI/main.cpp
+++ WinAPI/WinAPI/main.cpp
@@ -1,122 +1,113 @@
 ﻿#include<Windows.h>
 #include"resource.h"
 #include<CommCtrl.h>
+
 CONST CHAR g_sz_LOGIN_INVITATION[] = "Введите имя пользователя";
-//#define UNICODE
+
+//Процедура окна:
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
-    //hInstance - Экземпляр запущенного *.exe-файла нашей программы
-    //hPrevInst - не используется
-    //LPSTR - LongPointer To String
-    //lpCmdLine - CommandLine (командная строка с параметрами запуска приложения) 
-    //nCmdShow - режим отображения окна (развернуто на весь экран, cвернуто на панель задач)
-    //           n - Number
-    //          lp - Long Pointer
-    //           h - HINSTANCE
+	//hInstance - экземпляр запущенного *.exe-файла нашей программы
+	//hPrevInst - НЕ используется
+	//LPSTR - Long Pointer To String
+	//lpCmdLine - CommandLine (командная строка с параметрами запуска приложения)
+	//nCmdShow  - режим отображения окна (развернуто на весь экран, cвернуто на панель задач и т.д.)
+	//Префиксы: n..., lp... это Венгерская нотация
+	//			n - Number
+	//			lp - Long Pointer
+	//			h - HINSTANCE
 
-    /*MessageBox(NULL, "Hello Windows!\nThis is MessageBox()", "Window title", MB_CANCELTRYCONTINUE | MB_HELP | MB_DEFBUTTON2 | MB_ICONINFORMATION | MB_RIGHT);*/
+	/*MessageBox
+	(
+		NULL,
+		"Hello Windows!\nThis is MessageBox()",
+		"Window title",
+		MB_CANCELTRYCONTINUE | MB_HELP | MB_DEFBUTTON3 |
+		MB_ICONINFORMATION |
+		MB_SERVICE_NOTIFICATION |
+		MB_RIGHT
+	);*/
 
-    DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), 0, (DLGPROC)DlgProc, 0);
+	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), 0, (DLGPROC)DlgProc, 0);
 
-    return 0;
+	return 0;
 }
 
+//Процедура окна:
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-    //hwnd - Handler to Window. Обработчик или дескриптор окна - это число при помощи которого можно обратиться к окну.
-    //uNsg - Message. Сообшение, кторое отправляется окну
-    //wParam, lParam - это параметры сообщения, у каждого сообщения свои набор парамтеров.
-    switch (uMsg)
-    {
-    case WM_INITDIALOG:  // сообщение отправляется  раз при инициализации окна.
-    {
-        HWND hedit = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
-        SendMessage(hEdit, WM_SETTEXT, 0, g_sz_LOGIN_INVITATION);
-    }
-        break;
-    {
+	//hwnd - Handler to Window. Обработчик или дескриптор окна - это число, при помощи которого можно обратиться к окну.
+	//uMsg - Message. Сообщение, которое отправляется окну.
+	//wParam, lParam - это параметры сообщения, у каждого сообщения свой набор параметров.
+	//LOWORD - Младшее слово;
+	//HIWORD - Старшее слово;
+	//DWORD  - Double Word;
 
-        /*// плейсхолдер для поля 'Login'
-        HWND heditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
-        SendMessage(heditLogin, EM_SETCUEBANNER, 0, (LPARAM)TEXT("Введите имя пользователя"));*/
-    }
-    //return TRUE;
-        /*// Создаем буфер
-        CONST INT SIZE = 256;
-        CHAR sz_buffer[SIZE] = "Введите имя пользователя";
-        //CHAR sz_buffer[SIZE] = {}; //sz - String Zero (NULL terminated Line - C-string)
-        //TCHAR Messege = CHAR("Введите имя пользователя");
-        // Устанавливаем плейсхолдер для поля 'Login'
-        HWND heditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
-        SendMessage(heditLogin, WM_GETTEXT, SIZE, (LPARAM)Messege);
-        // Записываем полученный текст в текстовое поле 'Login':
-        SendMessage(heditLogin, WM_SETTEXT, SIZE, (LPARAM)Messege);
-    }
-        break;*/
-    case WM_COMMAND:   // обрабатывает нажатие кнопок и друг действия пользователя
-        switch (LOWORD(wParam))
-        {
-            case IDC_EDIT_LOGIN
-            CONST INT SIZE = 256;
-            CHAR sz_buffer[SIZE]{};
-            SendMessage((HWND)lParam, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
-            if (HIWORD(wParam) == EN_SETFOCUS)
-            {
-                if(strcmp(sz_buffer, g_sz_LOGIN_INVITATION) ==0)
-                    SendMessage((HWND)lParam, WM_SETTEXT, 0, (LPARAM)"");
-            }
-            if (HIWORD(wParam) == EN_KILLFOCUS)
-            {
-                if (strcmp(sz_buffer, "") == 0)
-                    SendMessage((HWND)lParam, WM_SETTEXT, 0, (LPARAM)g_sz_LOGIN_INVITATION);
-            }
-            break;
-        
-        case IDC_BUTTON_COPY:
-        {
-            // 1) Создаем буфер
-            CONST INT SIZE = 256;
-            CHAR sz_buffer[SIZE] = {}; //sz - String Zero (NULL terminated Line - C-string)
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:	//Это сообшение отправляется 1 раз при инициализации окна
+	{
+		HWND hEdit = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
+		SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)g_sz_LOGIN_INVITATION);
+	}
+	break;
+	case WM_COMMAND:	//Обрабатывает нажатие кнопок и другие действия пользователя
+		//ResourceID
+		switch (LOWORD(wParam))
+		{
+		case IDC_EDIT_LOGIN:
+		{
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE]{};
+			SendMessage((HWND)lParam, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+			/*
+			-----------------
+			WM_COMMAND
+			LOWORD(wParam) = ResourceID
+			HIWORD(wParam) = NotificationCode(EN_SETFOCUS)
+			-----------------
+			*/
+			if (HIWORD(wParam) == EN_SETFOCUS && strcmp(sz_buffer, g_sz_LOGIN_INVITATION) == 0)
+				SendMessage((HWND)lParam, WM_SETTEXT, 0, (LPARAM)"");
+			if (HIWORD(wParam) == EN_KILLFOCUS && strcmp(sz_buffer, "") == 0)
+				SendMessage((HWND)lParam, WM_SETTEXT, 0, (LPARAM)g_sz_LOGIN_INVITATION);
+			//EN_ - Edit Notofocation
+			/*
+			-----------------------------
+			Функция strcmp(const char* str1, const char* str2) сравнивает строки и возвращает значение типа 'int':
+				0  - строки идентичны;
+				!0 - строки отличаются;
+			-----------------------------
+			//https://legacy.cplusplus.com/reference/cstring/strcmp/
+			*/
+		}
+		break;
+		case IDC_BUTTON_COPY:
+		{
+			//1) Создаем буфер:
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE] = {};	//sz_ - String Zero (NULL Terminated Line - C-string)
 
-            // 2) Получаем обработчик текстовых полей:
-            HWND heditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
-            HWND heditPassword = GetDlgItem(hwnd, IDC_EDIT_PASSWORD);
+			//2) Получаем обработчики текстовых полей:
+			HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
+			HWND hEditPassword = GetDlgItem(hwnd, IDC_EDIT_PASSWORD);
 
-            // 3) Считываем содержимое поля 'Login' в буфер:
-            SendMessage(heditLogin, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+			//3) Считываем содержимое поля 'Login' в буфер:
+			SendMessage(hEditLogin, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
 
-            // 4) Записываем полученный текст в текстовое поле 'Password':
-            SendMessage(heditPassword, WM_SETTEXT, 0, (LPARAM)sz_buffer);
-
-        }
-        break;
-        case IDOK:MessageBox(hwnd, "Была нажата кнопка ОК", "Info", MB_OK | MB_ICONINFORMATION);
-            break;
-        case IDCANCEL:MessageBox(hwnd, "Была нажата кнопка ОТМЕНА", "Info", MB_OK | MB_ICONMASK); EndDialog(hwnd, 0); break;
-        }
-        break;
-    case WM_CLOSE:   // отправляет при нажатии кнопки закрыть, ЗАКРЫТЬ, "X"
-        EndDialog(hwnd, 0);
-        break;
-    }
-    return FALSE;
-}
-LRESULT CALLBACK LoginEditProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
-{
-    switch (uMsg)
-    {
-    case WM_SETTEXT:
-        if (LOWORD(wParam) == 0)
-        {
-            SetWindowText(hwnd, "Введите имя пользовттеля");
-        }
-        else
-        {
-            SetWindowText(hwnd, NULL);
-        }
-        break;
-    }
-    return CallWindowProc((WNDPROC)GetWindowLongPtr(hwnd, GWLP_WNDPROC), hwnd, uMsg, wParam, lParam);    // обработка родит-ого окна
+			//4) Записываем полученный текст в текстовое поле 'Password':
+			SendMessage(hEditPassword, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+		}
+		break;
+		case IDOK:		MessageBox(hwnd, "Была нажата кнопка OK", "Info", MB_OK | MB_ICONINFORMATION); break;
+		case IDCANCEL:	EndDialog(hwnd, 0); break;
+		}
+		break;
+	case WM_CLOSE:		//Отправляется при нажатии кнопки 'Закрыть' X
+		EndDialog(hwnd, 0);
+		break;
+	}
+	return FALSE;
 }
