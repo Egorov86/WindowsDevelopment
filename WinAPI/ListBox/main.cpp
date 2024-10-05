1031ba2611978cfd1e85c47363899d5eeb43163c
Egoro_st25@top-academy.ru <Egoro_st25@top-academy.ru>
2024-10-05 21:00:22 +0300

    Edit

--- WinAPI/ListBox/main.cpp
+++ WinAPI/ListBox/main.cpp
@@ -2,15 +2,16 @@
 #include<Windows.h>
 #include<cstdio>
 #include"resource.h"
+#include<CommCtrl.h>
 
-CONST CHAR* g_LIST_BOX_ITEMS[] = { "One", "Two", "Three", "Four", "Five", "Six" };
+CONST CHAR* g_LIST_BOX_ITEMS[] = { "This", "is", "my", "first", "List", "Box" };
 
-BOOL CALLBACK DlgProcLB(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
-BOOL CALLBACK DlgProcAdd(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+BOOL CALLBACK DlgProcAdd(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam); //подкл окна
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
-	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)DlgProcLB, 0);
+	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)DlgProc, 0);
 	/*DWORD dwErrorMessageID = GetLastError();
 	LPSTR lpszMessageBuffer;
 	FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
@@ -25,20 +26,20 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 	return 0;
 }
 
-BOOL CALLBACK DlgProcLB(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
 	switch (uMsg)
 	{
 	case WM_INITDIALOG:
 	{
-		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
-		SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
+		/*HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
+		SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);*/
 		HWND hCombo = GetDlgItem(hwnd, IDC_LIST1); // получили элемент дочернего элемента окна 
 		for (int i = 0; i < sizeof(g_LIST_BOX_ITEMS) / sizeof(g_LIST_BOX_ITEMS[0]); i++)
 		{
 			SendMessage(hCombo, LB_ADDSTRING, 0, (LPARAM)g_LIST_BOX_ITEMS[i]);
 		}
-		//SendMessage(hList, LB_SETCURSEL, 0, 0); // для изначального выбьора в окне
+		SendMessage(hCombo, LB_SETCURSEL, 0, 0); // для изначального выбьора в окне
 		
 	}
 	break;
@@ -46,9 +47,28 @@ BOOL CALLBACK DlgProcLB(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
         switch (LOWORD(wParam))
         {
 		case IDC_BUTTON_ADD:
-			//GetModuleHandle(NULL) - возвращает hInstance запущенноц програмимы
-			DialogBoxParam(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_DIALOG_ADD_ITEM), hwnd, DlgProcAdd, 0);
+			//GetModuleHandle(NULL) - возвращает hInstance запущенной програмимы
+			DialogBoxParam(GetModuleHandle(NULL), MAKEINTRESOURCE (IDD_DIALOG_ADD_ITEM), hwnd, DlgProcAdd, 0); // для появления второго окна
 				break;
+		case IDC_BUTTON_DELETE:  // удаляю пункт из списка
+		{
+			HWND hList = GetDlgItem(hwnd, IDC_LIST1);
+			INT iSelectedItem = SendMessage(hList, LB_GETCURSEL, 0, 0);
+			if (iSelectedItem != LB_ERR)
+			{
+				SendMessage(hList, LB_DELETESTRING, iSelectedItem, 0);
+			}
+		}
+		break;
+		case IDC_BUTTON_EDIT:  // удаляю пункт из списка
+		{
+			HWND hList = GetDlgItem(hwnd, IDC_LIST1);
+			INT iSelectedItem = SendMessage(hList, LB_GETCURSEL, 0, 0);
+			if (iSelectedItem != LB_ERR)
+			{
+				SendMessage(hList, LB_DELETESTRING, iSelectedItem, 0);
+			}
+		}
 		case IDOK:
 		{
 			//HWND hList1 = GetDlgItem(hwnd, IDC_LIST1);
@@ -56,11 +76,24 @@ BOOL CALLBACK DlgProcLB(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			CHAR sz_buffer[SIZE]{};
 			CHAR sz_message[SIZE] = "Вы ничего не выбрали.";
 			HWND hCombo = GetDlgItem(hwnd, IDC_LIST1);
-			INT i = SendMessage(hCombo, CB_GETCURSEL, 0,0);
+			INT i = SendMessage(hCombo, LB_GETCURSEL, 0, 0);
 			SendMessage(hCombo, LB_GETTEXT, i, (LPARAM)sz_buffer);
-			if(i!=LB_ERR)
-				sprintf(sz_message, "Вы выбрали элемент №%i со значением %s", i, sz_buffer);
+
+			if (i != LB_ERR)
+				sprintf(sz_message, "Вы выбрали элемент №%i со значением %s.", i, sz_buffer);
 			MessageBox(hwnd, sz_message, "Info", MB_OK | MB_ICONINFORMATION);
+
+			/*CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE]{};
+			CHAR sz_message[SIZE] = "Вы ничего не выбрали.";
+			HWND hCombo = GetDlgItem(hwnd, IDC_LIST1);
+			INT i = SendMessage(hCombo, LB_GETCURSEL, 0, 0);
+			SendMessage(hCombo, LB_GETTEXT, i, (LPARAM)sz_buffer);
+
+			if (i != LB_ERR)
+				sprintf(sz_message, "Вы выбрали элемент №%i со значением %s.", i, sz_buffer);
+
+			MessageBox(hwnd, sz_message, "Info", MB_OK | MB_ICONINFORMATION);*/
 
 			/*HWND hCombo = GetDlgItem(hwnd, IDC_LIST1);
 			INT i = SendMessage(hCombo, CB_GETCURSEL, 0, 0);
@@ -76,9 +109,7 @@ BOOL CALLBACK DlgProcLB(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			MessageBox(hwnd, sz_message, "Info", MB_OK | MB_ICONINFORMATION);*/
 		}
 		break;
-	case IDCANCEL:	MessageBox(hwnd, "Была нажата кнопка OТМЕНА", "Info", MB_OK | MB_ICONEXCLAMATION); EndDialog(hwnd, 0);
-		break;
-		
+	case IDCANCEL:	MessageBox(hwnd, "Была нажата кнопка OТМЕНА", "Info", MB_OK | MB_ICONINFORMATION); EndDialog(hwnd, 0);
 		}
 		break;
 	    case WM_CLOSE:
@@ -99,17 +130,22 @@ BOOL CALLBACK DlgProcAdd(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		{
 			CONST INT SIZE = 256;
 			CHAR sz_buffer[SIZE]{};
-			SendMessage(GetDlgItem(hwnd, IDC_EDIT_NAME), WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
-			HWND hParent = GetParent(hwnd);
-			HWND hListBox = GetDlgItem(hParent, IDC_LIST1);
-			if (SendMessage(hListBox, LB_FINDSTRING, -1, (LPARAM)sz_buffer)==LB_ERR)
-				SendMessage(hListBox, LB_ADDSTRING, 0, (LPARAM)sz_buffer);
-			else
-				MessageBox(hwnd, "Такое вхождение уже существует ", "Info", MB_OK | MB_ICONINFORMATION);
+			GetWindowText(GetDlgItem(hwnd, IDC_EDIT_NAME), sz_buffer, SIZE); // (нужна для того чтобы проверить текст на пустоту)
+			if (sz_buffer[0] != '\0') //если текс не равен детерминирующему нулю, то проверяю только на совпадение
+			{
+				HWND hParent = GetParent(hwnd);
+				HWND hListBox = GetDlgItem(hParent, IDC_LIST1);
+				if (SendMessage(hListBox, LB_FINDSTRING, -1, (LPARAM)sz_buffer) == LB_ERR)
+					SendMessage(hListBox, LB_ADDSTRING, 0, (LPARAM)sz_buffer);
+				else
+					MessageBox(hwnd, "Такое вхождение уже существует ", "Info", MB_OK | MB_ICONINFORMATION);
+			}
+			else   //если текс отсутсвует-> сообщ и выход.
+				MessageBox(hwnd, "Вы ничего не ввели ", "Info", MB_OK | MB_ICONHAND);
+			EndDialog(hwnd, IDOK);
 		}
 			break;
-		case IDCANCEL:
-			EndDialog(hwnd, 0);
+		case IDCANCEL:MessageBox(hwnd, "Была нажата кнопка OТМЕНА", "Info", MB_OK | MB_ICONINFORMATION); EndDialog(hwnd, 0);
 		}
 		break;
 	case WM_CLOSE:
--- WinAPI/ListBox/resource.h
+++ WinAPI/ListBox/resource.h
@@ -9,8 +9,9 @@
 #define IDC_EDIT_NAME                   1002
 #define IDC_STATIC_ADD_ITEM             1003
 #define IDC_BUTTON_ADD                  1004
-#define IDC_BUTTON2                     1005
 #define IDC_BUTTON_DELETE               1005
+#define IDC_BUTTON1                     1006
+#define IDC_BUTTON_EDIT                 1006
 
 // Next default values for new objects
 // 
@@ -18,7 +19,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        107
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1006
+#define _APS_NEXT_CONTROL_VALUE         1007
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
