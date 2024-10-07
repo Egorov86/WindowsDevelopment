2e77e6d164c462a71b744d69875f6f435d1489e4
Egoro_st25@top-academy.ru <Egoro_st25@top-academy.ru>
2024-10-07 21:32:55 +0300

    encoding

---
+++ WinAPI/MainWindow/main.cpp
@@ -0,0 +1,78 @@
+﻿# include<Windows.h>
+
+CONST CHAR g_sz_WINDOW_CLASS[] = "Main Windows"
+
+INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+INT WINAPI WinMAIN(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
+{
+	WNDCLASS wClass;
+	ZeroMemory(&wClass, sizeof(wClass));
+	wClass.style = NULL;
+	wClass.cbSize = sizeof(wClass);  // cb - count Byte
+	wClass.cbClsExtra = 0;
+	wClass.cbWndExtra = 0;
+
+	//Appearance:
+	wClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wClass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+	wClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wClass.hbrBackground = (HBRUSH)COLOR_WINDOW;
+
+
+	//
+	wClass.hInstance = hInstance;
+	wClass.lpfnWndProc = (WNDPROC)WndProc;
+	wClass.lpszClassName = g_sz_WINDOW_CLASS;
+	wClass.lpszMenuName = NULL;
+
+	if (RegisterClassEx(&wClass) == NULL)
+	{
+		MessageBox(NULL, "Class registration failed", "ERROR", MB_OK | MB_ICONINFORMATION);
+		return 0;
+	}
+
+	HWND hwnd = CreateWindowEx
+	{
+		NULL,
+		g_sz_WINDOW_CLASS,
+		g_sz_WINDOW_CLASS,
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		NULL,
+		NULL, / hMenu
+		hInstance,
+		NULL
+
+	};
+	if (hwnd == NULL)
+	{
+		MessageBox(NULL, "Window creation failed", "Error", MB_OK | MB_ICONERROR);
+		return 0;
+	}
+	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd); //прорисовыввает окно
+
+	MSG msg;
+	while (GetMessage(&msg, 0, 0, 0) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return msg.message;
+}
+
+INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_CREATE:
+		break;
+	case WM_COMMAND:
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+	}
+};
