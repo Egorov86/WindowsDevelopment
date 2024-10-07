382f0d6cb3b38f30148e11c73a9f8e286e22c9fb
Egoro_st25@top-academy.ru <Egoro_st25@top-academy.ru>
2024-10-07 20:34:07 +0300

    List encoding

--- WinAPI/ListBox/main.cpp
+++ WinAPI/ListBox/main.cpp
@@ -8,6 +8,7 @@ CONST CHAR* g_LIST_BOX_ITEMS[] = { "This", "is", "my", "first", "List", "Box" };
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 BOOL CALLBACK DlgProcAdd(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam); //подкл окна
+BOOL CALLBACK DlgProcChange(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
@@ -46,6 +47,18 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	case WM_COMMAND:
         switch (LOWORD(wParam))
         {
+		case IDC_LIST1:
+	    {
+			if (HIWORD(wParam) == LBN_DBLCLK)
+			{
+				//CONST INT SIZE = 256;
+				//CHAR sz_buffer[SIZE];
+				//INT i = SendMessage((HWND)lParam, LB_GETCURSEL, 0, 0);
+				//SendMessage((HWND)lParam, LB_GETTEXT, SIZE, (LPARAM)sz_buffer);
+				DialogBoxParam(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_DIALOG_ADD_ITEM), hwnd, DlgProcChange, 0);
+            }
+	    }
+		break;
 		case IDC_BUTTON_ADD:
 			//GetModuleHandle(NULL) - возвращает hInstance запущенной програмимы
 			DialogBoxParam(GetModuleHandle(NULL), MAKEINTRESOURCE (IDD_DIALOG_ADD_ITEM), hwnd, DlgProcAdd, 0); // для появления второго окна
@@ -60,7 +73,7 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			}
 		}
 		break;
-		case IDC_BUTTON_EDIT1:  // рекдатировать пункт из списка 
+		/*case IDC_BUTTON_EDIT1:  // рекдатировать пункт из списка 
 		{
 			HWND hList = GetDlgItem(hwnd, IDC_LIST1);
 			INT iSelectedItem = SendMessage(hList, LB_GETCURSEL, 0, 0);
@@ -71,9 +84,9 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			/*if (iSelectedItem != LB_ERR)
 			{
 				SendMessage(hList, CBN_EDITCHANGE, iSelectedItem, 0);
-			}*/
+			}
 		}
-		break;
+		break;*/
 		case IDOK:
 		{
 			//HWND hList1 = GetDlgItem(hwnd, IDC_LIST1);
@@ -127,6 +140,9 @@ BOOL CALLBACK DlgProcAdd(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	switch (uMsg)
 	{
 	case WM_INITDIALOG:
+		SendMessage(hwnd, WM_SETTEXT, 0, (LPARAM)"Изменить");
+		SendMessage(GetDlgItem(hwnd, IDOK), WM_SETTEXT, 0, (LPARAM)"Сохранить");
+		SetFocus(GetDlgItem(hwnd, IDC_EDIT_NAME));
 		break;
 	case WM_COMMAND:
 		switch (LOWORD(wParam))
@@ -151,6 +167,65 @@ BOOL CALLBACK DlgProcAdd(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		}
 			break;
 		case IDCANCEL:MessageBox(hwnd, "Была нажата кнопка OТМЕНА", "Info", MB_OK | MB_ICONINFORMATION); EndDialog(hwnd, 0);
+		}
+		break;
+	case WM_CLOSE:
+		EndDialog(hwnd, 0);
+	}
+	return FALSE;
+}
+
+BOOL CALLBACK DlgProcChange(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:
+	{
+		SendMessage(hwnd, WM_SETTEXT, 0, (LPARAM)"Изменить");
+		SendMessage(GetDlgItem(hwnd, IDOK), WM_SETTEXT, 0, (LPARAM)"Сохранить");
+
+		CONST INT SIZE = 256;
+		CHAR sz_buffer[SIZE]{};
+		HWND hParent = GetParent(hwnd);
+		HWND hListBox = GetDlgItem(hParent, IDC_LIST1);
+		HWND hEdit = GetDlgItem(hwnd, IDC_EDIT_NAME);
+		INT i = SendMessage(hListBox, LB_GETCURSEL, 0, 0);
+		SendMessage(hListBox, LB_GETTEXT, i, (LPARAM)sz_buffer);
+		SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+		SendMessage(hEdit, EM_SETSEL, 0, SendMessage(hEdit, WM_GETTEXTLENGTH, 0, 0));
+		SetFocus(GetDlgItem(hwnd, IDC_EDIT_NAME));
+		//     A  - ANSI/ASCII (Multibyte)
+		//     W  - Unicode(Wide Character)
+
+	}
+		break;
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case IDOK:
+		{
+			HWND hEdit = GetDlgItem(hwnd, IDC_EDIT_NAME);
+			HWND hParent = GetParent(hwnd);
+			HWND hListBox = GetDlgItem(hParent, IDC_LIST1);
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE]{};
+			SendMessage(hEdit, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+			INT i =  SendMessage(hListBox, LB_GETCURSEL, 0, 0);
+			//SendMessage(hListBox, LB_SETITEMDATA, i, (LPARAM)sz_buffer);
+			if (SendMessage(hListBox, LB_FINDSTRING, -1, (LPARAM)sz_buffer) == LB_ERR)
+			{
+				SendMessage(hListBox, LB_DELETESTRING, i, 0);
+				SendMessage(hListBox, LB_INSERTSTRING, i, (LPARAM)sz_buffer);
+				EndDialog(hwnd, 0);
+			}
+			else
+			{
+				MessageBox(hwnd, "Такое значение уже есть в списке", "Info", MB_OK | MB_ICONINFORMATION);
+			}
+
+		}
+		break;
+		case IDCANCEL: EndDialog(hwnd, 0);
 		}
 		break;
 	case WM_CLOSE:
