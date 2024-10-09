6dd4b613666864277205758a5a2c861d17756bde
Egoro_st25@top-academy.ru <Egoro_st25@top-academy.ru>
2024-10-09 21:28:01 +0300

    Calc encoding

--- WinAPI/MainWindow/main.cpp
+++ WinAPI/MainWindow/main.cpp
@@ -1,5 +1,9 @@
 ﻿#include<Windows.h>
 
+#define IDC_STATIC 1000   // 1 Создаём ResourseID для дочернего элемента
+#define IDC_EDIT   1002
+#define IDC_BUTTON 1003
+
 //MDI - Multi Document Interface
 
 CONST CHAR g_sz_WINDOW_CLASS[] = "Main Windows";	//Имя класса окна.
@@ -9,19 +13,31 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
 	//1) Регистрация класса окна:
-	//1.1. Создать и проинициализировать структуру WNDCLASSEX
+	//1.1 Создать и проинициализировать структуру WNDCLASSEX
 	WNDCLASSEX wClass;
 	ZeroMemory(&wClass, sizeof(wClass));
 
 	wClass.style = NULL;
-	wClass.cbSize = sizeof(wClass);	//cb - Count Bytes
-	wClass.cbClsExtra = 0;	        //Class Extra Bytes
-	wClass.cbWndExtra = 0;	        //Window Extra Bytes
+	wClass.cbSize = sizeof(wClass);	 //cb - Count Bytes
+	wClass.cbClsExtra = 0;	         //Class Extra Bytes
+	wClass.cbWndExtra = 0;	         //Window Extra Bytes
 
 	//Appearance:
-	wClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-	wClass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);	//Small Icon
-	wClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	//wClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	//wClass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);	//Small Icon
+	wClass.hIcon = (HICON)LoadImage(hInstance, "ICO\\Insta.ico", IMAGE_ICON, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE);
+	wClass.hIcon = (HICON)LoadImage(hInstance, "ICO\\netflix.ico", IMAGE_ICON, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE);
+
+	//wClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wClass.hCursor = (HCURSOR)LoadImage
+	(
+		hInstance,
+		"CUR\\Busy.cur",   /* если есть ещё папка то прописать через\\ */
+		IMAGE_CURSOR,
+		LR_DEFAULTSIZE,
+		LR_DEFAULTSIZE,
+		LR_LOADFROMFILE
+	);
 	wClass.hbrBackground = (HBRUSH)COLOR_WINDOW;
 
 	//
@@ -43,7 +59,8 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 		NULL,	                       //Window ExStyle
 		g_sz_WINDOW_CLASS,	           //Window Class Name
 		g_sz_WINDOW_CLASS,	           //Window Title
-		WS_OVERLAPPEDWINDOW,           //Window Style
+		//WS_OVERLAPPEDWINDOW,           //Window Style
+		WS_OVERLAPPED |WS_THICKFRAME | WS_SYSMENU | WS_MINIMIZEBOX | WS_MAXIMIZEBOX,           //Window Style
 		CW_USEDEFAULT, CW_USEDEFAULT,	//Coordinates
 		CW_USEDEFAULT, CW_USEDEFAULT,	//Window size
 		NULL,	//Parent
@@ -77,8 +94,55 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	switch (uMsg)
 	{
 	case WM_CREATE:
+		CreateWindowEx
+		(
+			NULL,
+			"Static",
+			"Это static_text, детка",
+			WS_CHILD | WS_VISIBLE, //Для всех дочер окон стиль обязательно булет  WS_CHILD и WS_VISIBLE (это как минимум)
+			100, 10,
+			200, 20,
+			hwnd,
+			(HMENU)IDC_STATIC,
+			GetModuleHandle(NULL),
+			NULL
+		);
+		CreateWindowEx(
+			NULL, "Edit", "",
+			WS_CHILD | WS_VISIBLE | WS_BORDER, 
+			100, 30,
+			200, 20,
+			hwnd,
+			(HMENU)IDC_EDIT,
+			GetModuleHandle(NULL),
+			NULL
+		);
+		CreateWindowEx(
+			NULL, "Button", "Применить",
+			WS_CHILD | WS_VISIBLE,
+			200, 55,
+			100, 32,
+			hwnd,
+			(HMENU)IDC_BUTTON,
+			GetModuleHandle(NULL),
+			NULL);
 		break;
 	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case IDC_BUTTON:
+		{
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE]{};
+			HWND hedit = GetDlgItem(hwnd, IDC_EDIT);
+			SendMessage(hedit, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+
+			HWND hStatic = GetDlgItem(hwnd, IDC_STATIC);
+			SendMessage(hStatic, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+			SendMessage(hwnd, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+		}
+		break;
+		}
 		break;
 	case WM_DESTROY:
 		PostQuitMessage(0);
