f68589f0a0089c192e4469166c7e9ec58c8e918b
Egoro_st25@top-academy.ru <Egoro_st25@top-academy.ru>
2024-10-16 21:27:11 +0300

    encoding

--- WindowsForms/Clock/MainForm.Designer.cs
+++ WindowsForms/Clock/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Clock
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -29,9 +29,12 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.labeltime = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.cbShowDate = new System.Windows.Forms.CheckBox();
+            this.btnHideControls = new System.Windows.Forms.Button();
+            this.notifyIconSystemTray = new System.Windows.Forms.NotifyIcon(this.components);
             this.SuspendLayout();
             // 
             // labeltime
@@ -44,6 +47,7 @@
             this.labeltime.TabIndex = 0;
             this.labeltime.Text = "Time";
             this.labeltime.Click += new System.EventHandler(this.labeltime_Click);
+            this.labeltime.DoubleClick += new System.EventHandler(this.labeltime_DoubleClick);
             // 
             // timer1
             // 
@@ -63,15 +67,40 @@
             this.cbShowDate.Text = "Показать дату";
             this.cbShowDate.UseVisualStyleBackColor = true;
             // 
-            // Form1
+            // btnHideControls
+            // 
+            this.btnHideControls.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnHideControls.Location = new System.Drawing.Point(34, 203);
+            this.btnHideControls.Name = "btnHideControls";
+            this.btnHideControls.Size = new System.Drawing.Size(238, 82);
+            this.btnHideControls.TabIndex = 2;
+            this.btnHideControls.Text = "Скрыть элементы управления";
+            this.btnHideControls.UseVisualStyleBackColor = true;
+            this.btnHideControls.Click += new System.EventHandler(this.btnHideControls_Click);
+            // 
+            // notifyIconSystemTray
+            // 
+            this.notifyIconSystemTray.BalloonTipTitle = "Current time";
+            this.notifyIconSystemTray.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIconSystemTray.Icon")));
+            this.notifyIconSystemTray.Text = "notifyIcon1";
+            this.notifyIconSystemTray.Visible = true;
+            this.notifyIconSystemTray.MouseMove += new System.Windows.Forms.MouseEventHandler(this.notifyIconSystemTray_MouseMove);
+            // 
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 326);
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(356, 450);
+            this.Controls.Add(this.btnHideControls);
             this.Controls.Add(this.cbShowDate);
             this.Controls.Add(this.labeltime);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Clock";
+            this.TransparencyKey = System.Drawing.Color.White;
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -82,6 +111,8 @@
         private System.Windows.Forms.Label labeltime;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.CheckBox cbShowDate;
+        private System.Windows.Forms.Button btnHideControls;
+        private System.Windows.Forms.NotifyIcon notifyIconSystemTray;
     }
 }
 
---
+++ WindowsForms/Clock/MainForm.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clock
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            this.TransparencyKey = Color.Empty;
+        }
+
+        private void labeltime_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            labeltime.Text = DateTime.Now.ToString("HH:mm:ss tt");
+            if(cbShowDate.Checked)
+            {
+                labeltime.Text += $"\n{DateTime.Today.ToString("yyyy.MM.dd")}";
+            }
+            //notifyIconSystemTray.BalloonTipText = "Current time" + labeltime.Text;
+        }
+        private void setVisibility(bool visible)
+        {
+            this.TransparencyKey = visible ? Color.Empty :this.BackColor;
+            this.FormBorderStyle = visible ? FormBorderStyle.Sizable: FormBorderStyle.None;
+            this.ShowInTaskbar = visible;
+            cbShowDate.Visible = visible;
+            btnHideControls.Visible = visible;
+            labeltime.BackColor = visible ? Color.Empty : Color.Coral;
+        }
+        private void btnHideControls_Click(object sender, EventArgs e)
+        {
+            setVisibility(false);
+            notifyIconSystemTray.ShowBalloonTip(1, "Важная инфа", "надо нажать два раза", ToolTipIcon.Error);
+        }
+
+        private void labeltime_DoubleClick(object sender, EventArgs e)
+        {
+            setVisibility(true);
+            
+        }
+        private void notifyIconSystemTray_MouseMove(object sender, MouseEventArgs e)
+        {
+            notifyIconSystemTray.Text = "Current time:\n" + labeltime.Text;
+        }
+    }
+}
